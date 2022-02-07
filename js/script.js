cae7c9913580f98705e0aaaae02bba48d8cb2af3
philjang <pjang7390@gmail.com>
2022-02-07 01:05:04 -0800

    remove coin&npc intervals, incorporate into gameloop using time. dry up gamepiece code

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ console.log('hello')
 const canvas = document.getElementById('canvas')
 const timerDiv = document.getElementById('timer')
 const coinDiv = document.getElementById('coins')
+const subDiv = document.getElementById('sub')
 // object to store key status
 const keysDown = {}
 // key event listeners for pc movement
@@ -42,51 +43,35 @@ const timer = setInterval(changeSeconds, 1000)
 /* GAME FUNCTIONS */
 // resolution at x: 1510 ,y: 550
 
-// pc object
-const pc = {
-    xpos: 100,
-    ypos: 400,
-    width: 50,
-    height: 50,
-    color: 'blue',
-    // jumpGravity: 0,
-    jumpTime: 0,
-    render: () => {
-        ctx.fillStyle = pc.color
-        ctx.fillRect(pc.xpos, pc.ypos, pc.width, pc.height)
-    },
-    move: ()=> {
-        const speed = 10 // set increment value to move per keydown
-        if(keysDown.ArrowLeft) pc.xpos -= speed
-        if(keysDown.ArrowRight) pc.xpos += speed
-        if(pc.ypos === 400&&keysDown.ArrowUp) {
-            pc.jumpTime = 0.01
-            pc.ypos = 400+(-100*pc.jumpTime+10*pc.jumpTime**2)
-        }
-        if(pc.ypos<400) {
-            pc.jumpTime += 0.7
-            pc.ypos = 400+(-100*pc.jumpTime+10*pc.jumpTime**2)
-        }
-        if(pc.ypos === 400) pc.jumpTime = 0
+// // pc object
+// const pc = {
+//     xpos: 100,
+//     ypos: 400,
+//     width: 50,
+//     height: 50,
+//     color: 'blue',
+//     // jumpGravity: 0,
+//     jumpTime: 0,
+//     render: () => {
+//         ctx.fillStyle = pc.color
+//         ctx.fillRect(pc.xpos, pc.ypos, pc.width, pc.height)
+//     },
+//     move: ()=> {
+//         const speed = 10 // set increment value to move per keydown
+//         if(keysDown.ArrowLeft) pc.xpos -= speed
+//         if(keysDown.ArrowRight) pc.xpos += speed
+//         if(pc.ypos === 400&&keysDown.ArrowUp) {
+//             pc.jumpTime = 0.01
+//             pc.ypos = 400+(-100*pc.jumpTime+10*pc.jumpTime**2)
+//         }
+//         if(pc.ypos<400) {
+//             pc.jumpTime += 0.7
+//             pc.ypos = 400+(-100*pc.jumpTime+10*pc.jumpTime**2)
+//         }
+//         if(pc.ypos>400) pc.ypos = 400 // prevents overshoot from gravity
+//     }
 
-
-
-        // // reset gravity to 0 when pc touches ground
-        // if(pc.ypos === 400) {
-        //     pc.jumpGravity = 0
-        // }
-        // // function to add gravity once in air
-        // if(pc.ypos<400) { // airborne parameters
-        //     if(keysDown.ArrowUp) {
-        //         pc.jumpGravity += 2
-        //         pc.ypos += 20+pc.jumpGravity
-        //     } else pc.ypos += 15
-        // }
-        // if(keysDown.ArrowUp) pc.ypos -= 45
-        if(pc.ypos>400) pc.ypos = 400 // prevents overshoot from gravity
-    }
-
-}
+// }
 
 // // npc object -- test archetype
 // const npc = {
@@ -102,26 +87,83 @@ const pc = {
 // }
 
 // npc factory
-class npcMold {
-    static totalRendered = 0
-    constructor (inputH, inputC, inputS) {
-        this.xpos = 1500 // render at edge of screen
-        this.ypos = 450 - inputH // vertical position rendered
-        // size of npc
-        this.width = 50 
+// class npcMold {
+//     static totalRendered = -1
+//     constructor (inputH, inputC, inputS) {
+//         this.xpos = 1500 // render at edge of screen
+//         this.ypos = 450 - inputH // vertical position rendered
+//         // size of npc
+//         this.width = 50 
+//         this.height = inputH
+//         this.color = inputC // npc color
+//         this.speed = inputS // npc speed
+//         npcMold.totalRendered++
+//     }
+//     // method to render for each frame
+//     render() {
+//         ctx.fillStyle = this.color
+//         ctx.fillRect(this.xpos, this.ypos, this.width, this.height)
+//     }
+//     // dictates npc movement 
+//         move() {
+//         this.xpos -= this.speed
+//     }
+//     // checks for overlap of objects at each frame
+//     detectHit() {
+//         // compare relation of pc and this npc
+//         const pcRight = pc.xpos + pc.width >= this.xpos
+//         const pcLeft = pc.xpos <= this.xpos + this.width
+//         const pcTop = pc.ypos <= this.ypos + this.height
+//         const pcBottom = pc.ypos + pc.height >= this.ypos
+//         // if all 4 true, then possibility for hit
+//         if(pcRight&&pcLeft&&pcTop&&pcBottom) {
+//             console.log('hit detected with '+this.color) // check hitbox
+//             endGame()
+//         }
+//     }
+// }
+
+class gamePiece {
+    constructor (inputX, inputY, inputW, inputH, inputC, inputS) {
+        this.xpos = inputX
+        this.ypos = inputY
+        this.width = inputW
         this.height = inputH
-        this.color = inputC // npc color
-        this.speed = inputS // npc speed
-        npcMold.totalRendered++
+        this.color = inputC
+        this.speed = inputS
     }
     // method to render for each frame
     render() {
         ctx.fillStyle = this.color
         ctx.fillRect(this.xpos, this.ypos, this.width, this.height)
     }
-    // dictates npc movement 
-        move() {
+    // dictates movement
+    move() {
         this.xpos -= this.speed
+    }
+}
+let pc = new gamePiece(100, 400, 50, 50, 'blue', 10)
+pc.jumpTime = 0
+pc.move = ()=> {
+    const speed = 10 // set increment value to move per keydown
+    if(keysDown.ArrowLeft) pc.xpos -= speed
+    if(keysDown.ArrowRight) pc.xpos += speed
+    if(pc.ypos === 400&&keysDown.ArrowUp) {
+        pc.jumpTime = 0.01
+        pc.ypos = 400+(-100*pc.jumpTime+10*pc.jumpTime**2)
+    }
+    if(pc.ypos<400) {
+        pc.jumpTime += 0.7
+        pc.ypos = 400+(-100*pc.jumpTime+10*pc.jumpTime**2)
+    }
+    if(pc.ypos>400) pc.ypos = 400 // prevents overshoot from gravity
+}
+
+class npcMold extends gamePiece {
+    static totalRendered = -1
+    constructor (inputX, inputY, inputW, inputH, inputC, inputS) {
+        super(inputX, inputY, inputW, inputH, inputC, inputS)
+        npcMold.totalRendered++
     }
     // checks for overlap of objects at each frame
     detectHit() {
@@ -139,25 +181,49 @@ class npcMold {
 }
 
 // enemy types on screen at same time
-let npc1 = new npcMold(100,'red', 10)
-let npc2 = new npcMold(100,'green', 20)
+// let npc1 = new npcMold(100,'red', 10)
+// let npc2 = new npcMold(100,'green', 20)
+// first enemies
+let npc1 = new npcMold(1500, 350, 50, 100, 'red', 10)
+let npc2 = new npcMold(1500, 350, 50, 100, 'green', 20)
+// inputY = 450-desired height
 
-// coin factory -- same logic as npc, but collision = +coin
-class coinMold {
-    static coinsCollected = 0 
-    constructor (inputX, inputY) {
-        this.xpos = inputX
-        this.ypos = inputY
-        this.width = 20
-        this.height = 20
-        this.color = 'yellow'
-    }
-    render() {
-        ctx.fillStyle = this.color
-        ctx.fillRect(this.xpos, this.ypos, this.width, this.height)
-    }
-    move() {
-        this.xpos -= 5
+// // coin factory -- same logic as npc, but collision = +coin
+// class coinMold {
+//     static coinsCollected = 0 
+//     constructor (inputX, inputY) {
+//         this.xpos = inputX
+//         this.ypos = inputY
+//         this.width = 20
+//         this.height = 20
+//         this.color = 'yellow'
+//     }
+//     render() {
+//         ctx.fillStyle = this.color
+//         ctx.fillRect(this.xpos, this.ypos, this.width, this.height)
+//     }
+//     move() {
+//         this.xpos -= 5
+//     }
+//     detectHit() {
+//         const pcRight = pc.xpos + pc.width >= this.xpos
+//         const pcLeft = pc.xpos <= this.xpos + this.width
+//         const pcTop = pc.ypos <= this.ypos + this.height
+//         const pcBottom = pc.ypos + pc.height >= this.ypos
+//         if(pcRight&&pcLeft&&pcTop&&pcBottom) {
+//             coinMold.coinsCollected++
+//             console.log('hit detected with '+this.color)
+//             this.ypos = -100 // send coin off screen
+//             // console.log(coinMold.coinsCollected) // check total coin count
+//             // display coin count in div
+//             coinDiv.innerText = `${coinMold.coinsCollected} coins`
+//         }
+//     }
+// }
+class coinMold extends gamePiece {
+    static coinsCollected = 0
+    constructor (inputX, inputY, inputW, inputH, inputC, inputS) {
+        super(inputX, inputY, inputW, inputH, inputC, inputS)
     }
     detectHit() {
         const pcRight = pc.xpos + pc.width >= this.xpos
@@ -176,10 +242,11 @@ class coinMold {
 }
 
 // coins on screen at same time
-let coin1 = new coinMold(1490,200)
-let coin2 = new coinMold(1540,160)
-let coin3 = new coinMold(1610,160)
-let coin4 = new coinMold(1660,200)
+// first coins
+let coin1 = new coinMold(1490,200, 20, 20, 'yellow', 5)
+let coin2 = new coinMold(1540,160, 20, 20, 'yellow', 5)
+let coin3 = new coinMold(1610,160, 20, 20, 'yellow', 5)
+let coin4 = new coinMold(1660,200, 20, 20, 'yellow', 5)
 
 
 // // make character
@@ -220,6 +287,14 @@ function gameLoop () { // declaration allows global scope
     // start gameloop with clear screen
     ctx.clearRect(0,0, canvas.width, canvas.height)
     console.log(seconds)
+    if(seconds%5===0&&npc1.xpos<0)npc1 = new npcMold(1500, 350, 50, 100, 'red', 10)
+    if(seconds%5===0&&npc2.xpos<0)npc2 = new npcMold(1500, 350, 50, 100, 'green', 20)
+    if(seconds%15===0&&coin4.xpos<0) {
+        coin1 = new coinMold(1490,200, 20, 20, 'yellow', 5)
+        coin2 = new coinMold(1540,160, 20, 20, 'yellow', 5)
+        coin3 = new coinMold(1610,160, 20, 20, 'yellow', 5)
+        coin4 = new coinMold(1660,200, 20, 20, 'yellow', 5)
+    }
 
     // componentArr.forEach( e => e.render())
     // componentArr.forEach( (e,i) => {if(i>0) e.detectHit()})
@@ -248,33 +323,31 @@ function gameLoop () { // declaration allows global scope
     coin2.move()
     coin3.move()
     coin4.move()
-
 }
 
 
-// new npc when last is off screen
-const npcInterval = setInterval(npcCreator, 5000)
-function npcCreator () {
-    if(npc1.xpos<0) npc1 = new npcMold(100, 'red', 10)
-    if(npc2.xpos<0) npc2 = new npcMold(100, 'green', 20)
-    // console.log(npcMold.totalRendered) // check total npc avoided
-}
+// // new npc when last is off screen
+// const npcInterval = setInterval(npcCreator, 5000)
+// function npcCreator () {
+//     if(npc1.xpos<0) npc1 = new npcMold(100, 'red', 10)
+//     if(npc2.xpos<0) npc2 = new npcMold(100, 'green', 20)
+//     // console.log(npcMold.totalRendered) // check total npc avoided
+// }
 
-// new coins every 30 secodns
-const coinInterval = setInterval(coinCreator, 30000)
-function coinCreator () {
-    coin1 = new coinMold(1490,200)
-    coin2 = new coinMold(1540,160)
-    coin3 = new coinMold(1610,160)
-    coin4 = new coinMold(1660,200)
-}
+// // new coins every 30 secodns
+// const coinInterval = setInterval(coinCreator, 5000)
+// function coinCreator () {
+//     coin1 = new coinMold(1490,200)
+//     coin2 = new coinMold(1540,160)
+//     coin3 = new coinMold(1610,160)
+//     coin4 = new coinMold(1660,200)
+// }
 
 // end game
 function endGame() {
-    console.log(`GAME OVER... You avoided ${npcMold.totalRendered} enemies and collected ${coinMold.coinsCollected} coins.`)
+    subDiv.innerText = `GAME OVER... You avoided ${npcMold.totalRendered} enemies and collected ${coinMold.coinsCollected} coins.`
     clearInterval(gameInterval)
-    clearInterval(npcInterval)
-    clearInterval(coinInterval)
+    clearInterval(timer)
 }
 
 
