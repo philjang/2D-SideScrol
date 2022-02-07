de045e0f1db17957014336b8328ed775490bc7e7
philjang <pjang7390@gmail.com>
2022-02-07 12:22:24 -0800

    add function to initialize game on btn click and restart after lose

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
-// link check
-console.log('hello')
-
 /* DOM SELECTORS and EVENT LISTENERS */
 const canvas = document.getElementById('canvas')
 const timerDiv = document.getElementById('timer')
 const coinDiv = document.getElementById('coins')
 const subDiv = document.getElementById('sub')
+const startBtn = document.getElementById('start-btn')
 // object to store key status
 const keysDown = {}
 // key event listeners for pc movement
@@ -16,16 +14,22 @@ document.addEventListener('keyup', e => keysDown[e.key] = false )
 /* CANVAS RENDERING */
 const ctx = canvas.getContext('2d')
 
-// //research
-// // sets canvas to be the size the element takes up (improve resolution)
+// sets canvas to be the size the element takes up (improve resolution)
 canvas.setAttribute('width', getComputedStyle(canvas)["width"])
 canvas.setAttribute('height', getComputedStyle(canvas)["height"])
 
-// gameloop frequency
-const gameInterval = setInterval(gameLoop, 50)
+/* GAME FUNCTIONS */
+// resolution at x: 1510 ,y: 550
+
+// gameloop variables
+// time since start
+let seconds = ""
+// gameloop frequency, set in initialize()
+let gameInterval = ""
+// stopwatch interval
+let timer = ""
 
 // timer function
-let seconds = 0
 const changeSeconds = () => {
     seconds++
       // convert num to string for padStart
@@ -35,13 +39,44 @@ const changeSeconds = () => {
       // display timer in div
       timerDiv.innerText = masterTime
 }
-const timer = setInterval(changeSeconds, 1000)
 
-// console.log(canvas) // check canvas
-// console.log(ctx) // check context2D object
 
-/* GAME FUNCTIONS */
-// resolution at x: 1510 ,y: 550
+startBtn.addEventListener('click',initialize)
+function initialize() {
+    seconds = 0 // reset timer
+    coinMold.coinsCollected = 0 // reset coin count
+    timerDiv.innerText = "00:00" // reset time display
+    coinDiv.innerText = "" // reset coin display
+    subDiv.innerText = "" // clear game over message
+    pc = new gamePiece(100, 400, 50, 50, 'blue', 10)
+    pc.jumpTime = 0 // add property of jumpTime for gravity calculations
+    // player movement function as key-value pair
+    pc.move = ()=> {
+    const speed = 10 // set increment value to move per keydown
+    if(keysDown.ArrowLeft) pc.xpos -= speed
+    if(keysDown.ArrowRight) pc.xpos += speed
+    if(pc.ypos === 400&&keysDown.ArrowUp) {
+        pc.jumpTime = 0.01
+        pc.ypos = 400+(-100*pc.jumpTime+10*pc.jumpTime**2)
+    }
+    if(pc.ypos<400) {
+        pc.jumpTime += 0.7
+        pc.ypos = 400+(-100*pc.jumpTime+10*pc.jumpTime**2)
+    }
+    if(pc.ypos>400) pc.ypos = 400 // prevents overshoot from gravity
+}
+    // spawn first wave
+    npc1 = new npcMold(1500, 350, 50, 100, 'red', 10)
+    npc2 = new npcMold(1500, 350, 50, 100, 'green', 20)
+    coin1 = new coinMold(1490,200, 20, 20, 'yellow', 5)
+    coin2 = new coinMold(1540,160, 20, 20, 'yellow', 5)
+    coin3 = new coinMold(1610,160, 20, 20, 'yellow', 5)
+    coin4 = new coinMold(1660,200, 20, 20, 'yellow', 5)
+    // begin intervals for stopwatch and gameloop
+    gameInterval = setInterval(gameLoop, 50)
+    timer = setInterval(changeSeconds, 1000)
+    startBtn.style.display = 'none' // remove start btn from view
+}
 
 // // pc object
 // const pc = {
@@ -143,21 +178,21 @@ class gamePiece {
     }
 }
 let pc = new gamePiece(100, 400, 50, 50, 'blue', 10)
-pc.jumpTime = 0
-pc.move = ()=> {
-    const speed = 10 // set increment value to move per keydown
-    if(keysDown.ArrowLeft) pc.xpos -= speed
-    if(keysDown.ArrowRight) pc.xpos += speed
-    if(pc.ypos === 400&&keysDown.ArrowUp) {
-        pc.jumpTime = 0.01
-        pc.ypos = 400+(-100*pc.jumpTime+10*pc.jumpTime**2)
-    }
-    if(pc.ypos<400) {
-        pc.jumpTime += 0.7
-        pc.ypos = 400+(-100*pc.jumpTime+10*pc.jumpTime**2)
-    }
-    if(pc.ypos>400) pc.ypos = 400 // prevents overshoot from gravity
-}
+// pc.jumpTime = 0
+// pc.move = ()=> {
+//     const speed = 10 // set increment value to move per keydown
+//     if(keysDown.ArrowLeft) pc.xpos -= speed
+//     if(keysDown.ArrowRight) pc.xpos += speed
+//     if(pc.ypos === 400&&keysDown.ArrowUp) {
+//         pc.jumpTime = 0.01
+//         pc.ypos = 400+(-100*pc.jumpTime+10*pc.jumpTime**2)
+//     }
+//     if(pc.ypos<400) {
+//         pc.jumpTime += 0.7
+//         pc.ypos = 400+(-100*pc.jumpTime+10*pc.jumpTime**2)
+//     }
+//     if(pc.ypos>400) pc.ypos = 400 // prevents overshoot from gravity
+// }
 
 class npcMold extends gamePiece {
     static totalRendered = -1
@@ -281,12 +316,9 @@ let coin4 = new coinMold(1660,200, 20, 20, 'yellow', 5)
 //         console.log('hit detected') // check hitbox
 //     }
 // }
-const componentArr = [pc, npc1, npc2, coin1, coin2, coin3, coin4]
+// const componentArr = [pc, npc1, npc2, coin1, coin2, coin3, coin4]
 
-function gameLoop () { // declaration allows global scope
-    // start gameloop with clear screen
-    ctx.clearRect(0,0, canvas.width, canvas.height)
-    console.log(seconds)
+function spawn () {
     if(seconds%5===0&&npc1.xpos<0)npc1 = new npcMold(1500, 350, 50, 100, 'red', 10)
     if(seconds%5===0&&npc2.xpos<0)npc2 = new npcMold(1500, 350, 50, 100, 'green', 20)
     if(seconds%15===0&&coin4.xpos<0) {
@@ -295,6 +327,23 @@ function gameLoop () { // declaration allows global scope
         coin3 = new coinMold(1610,160, 20, 20, 'yellow', 5)
         coin4 = new coinMold(1660,200, 20, 20, 'yellow', 5)
     }
+    if(seconds%10) {
+        gameInterval = setInterval(gameLoop )
+    }
+}
+
+// end game
+function endGame() {
+    subDiv.innerText = `GAME OVER... You avoided ${npcMold.totalRendered} enemies and collected ${coinMold.coinsCollected} coins.`
+    clearInterval(gameInterval)
+    clearInterval(timer)
+    startBtn.style.display = 'inline-block'
+}
+
+function gameLoop () { // declaration allows global scope
+    // start gameloop with clear screen
+    ctx.clearRect(0,0, canvas.width, canvas.height)
+    spawn()
 
     // componentArr.forEach( e => e.render())
     // componentArr.forEach( (e,i) => {if(i>0) e.detectHit()})
@@ -343,12 +392,12 @@ function gameLoop () { // declaration allows global scope
 //     coin4 = new coinMold(1660,200)
 // }
 
-// end game
-function endGame() {
-    subDiv.innerText = `GAME OVER... You avoided ${npcMold.totalRendered} enemies and collected ${coinMold.coinsCollected} coins.`
-    clearInterval(gameInterval)
-    clearInterval(timer)
-}
+// // end game
+// function endGame() {
+//     subDiv.innerText = `GAME OVER... You avoided ${npcMold.totalRendered} enemies and collected ${coinMold.coinsCollected} coins.`
+//     clearInterval(gameInterval)
+//     clearInterval(timer)
+// }
 
 
 // document.addEventListener('DOMContentLoaded', {
